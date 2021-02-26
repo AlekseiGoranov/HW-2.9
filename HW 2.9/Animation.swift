27157b9dce611c7731143501cc6c40152ea9a782
Aleksei <goranov93@mail.ru>
2021-02-26 17:57:30 +0300

    did finding

--- HW 2.9/Animation.swift
+++ HW 2.9/Animation.swift
@@ -18,7 +18,7 @@ struct Animation {
         preset = Spring.AnimationPreset.allCases.randomElement()!.rawValue
         curve = Spring.AnimationCurve.allCases.randomElement()!.rawValue
         force = (CGFloat.random(in: 0...2) * 100).rounded() / 100
-        duration = (CGFloat.random(in: 0...3) * 100).rounded() / 100
+        duration = (CGFloat.random(in: 0.2...3) * 100).rounded() / 100
         delay = (CGFloat.random(in: 0.1...1) * 100).rounded() / 100
     }
 }
